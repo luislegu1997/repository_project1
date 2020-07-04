3172ae3992fce944acd89835e13958d80524055f
luislegu1997 <luislegu@hotmail.com>
2020-07-04 12:10:31 -0500

    added index links

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+
+
+app_name = "encyclopedia"
 urlpatterns = [
-    path("", views.index, name="index")
+    
+    path("", views.index, name="index"),
+    path("<str:title>", views.title, name="title"),
 ]
+
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import markdown2
 
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def title(request, title):
+    
+    response = util.get_entry(title)
+
+
+    if response is None:
+
+        return render(request, "encyclopedia/error.html", {
+            "message" : "requested page was not found", 
+        })
+
+
+    
+    return render(request, "encyclopedia/response.html",
+   {
+       "response" : markdown2.markdown(response),
+       "title" : title
+   })
+
+   # return HttpResponse("Hello, world!")
+
+
 
